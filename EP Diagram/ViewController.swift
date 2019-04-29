58c400b5657d734169c67d79922a7553f6596b77
David Mann <mannd@epstudiossoftware.com>
2019-04-29 16:09:10 -0600

    Add zoom

--- EP Diagram/ViewController.swift
+++ EP Diagram/ViewController.swift
@@ -11,16 +11,23 @@ import UIKit
 class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var ladderView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         title = "EP Diagram"
         scrollView.delegate = self
+        ladderView.backgroundColor = UIColor.gray
     }
 
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         print(scrollView.bounds)
+    }
+
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 
 }
