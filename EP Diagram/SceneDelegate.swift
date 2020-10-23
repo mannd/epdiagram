2fc211cc8c8e390e590c2c3f3a71e5a7d52ceaac
David Mann <mannd@epstudiossoftware.com>
2020-10-23 17:40:25 -0600

    Initial support for scenes

---
+++ EP Diagram/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  EP Diagram
+//
+//  Created by David Mann on 10/23/20.
+//  Copyright © 2020 EP Studios. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
